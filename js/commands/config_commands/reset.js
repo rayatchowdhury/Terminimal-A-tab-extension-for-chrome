19962c39d0a62e4988395937aa6813c274a0608e
Rayat Chowdhury <rayated.ray@gmail.com>
2024-11-30 20:23:21 +0600

    Replace defaultConfig.js with defaultConfig.json for improved configuration management; update reset command to fetch configuration from JSON file

--- js/commands/config_commands/reset.js
+++ js/commands/config_commands/reset.js
@@ -1,19 +1,24 @@
-
 import { render } from '../../helpers.js';
-import defaultConfig from './defaultConfig.js';
 
-const execute = () => {
-  window.config = JSON.parse(JSON.stringify(defaultConfig));
-  
-  // Update DOM
-  const root = document.documentElement;
-  root.style.setProperty('--font-family', defaultConfig.term.font);
-  root.style.setProperty('--font-size', defaultConfig.term.size);
-  root.style.setProperty('--terminal-opacity', defaultConfig.style.term_opacity);
-  root.style.setProperty('--shortcuts-opacity', defaultConfig.style.menu_opacity);
-  root.style.setProperty('--background-blur', defaultConfig.style.bg.blur);
-  
-  render('<span class="green">Configuration reset to defaults</span>');
+const execute = async () => {
+  try {
+    const response = await fetch(chrome.runtime.getURL('defaultConfig.json'));
+    const defaultConfig = await response.json();
+    
+    window.config = JSON.parse(JSON.stringify(defaultConfig));
+    
+    // Update DOM
+    const root = document.documentElement;
+    root.style.setProperty('--font-family', defaultConfig.term.font);
+    root.style.setProperty('--font-size', defaultConfig.term.size);
+    root.style.setProperty('--terminal-opacity', defaultConfig.style.term_opacity);
+    root.style.setProperty('--shortcuts-opacity', defaultConfig.style.menu_opacity);
+    root.style.setProperty('--background-blur', defaultConfig.style.bg.blur);
+    
+    render('<span class="green">Configuration reset to defaults</span>');
+  } catch (err) {
+    render('<span class="red">Error resetting configuration</span>');
+  }
 };
 
 export default { execute };
